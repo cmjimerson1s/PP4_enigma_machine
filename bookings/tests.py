8c63f32f241a44b55cbc16d358e616c37b8da110
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-16 13:13:47 +0000

    Install coverage and begin testing

--- bookings/tests.py
+++ bookings/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Reservation
+from . import views
+from django.urls import reverse
 
-# Create your tests here.
+
+
+class BlogPostListViewTest(TestCase):
+
+    def setUp(self):
+        self.reservation = Reservation.objects.create(
+            customer_name = 'Test Test',
+            customer_email = 'test@gmail.com',
+            customer_phone = '+123456789123',
+            price = 500,
+            date = '2023-01-01',
+            time_slot = '14:00',
+            room_choice = 'Horror',
+            comment = 'Test',
+            user_id =  55,
+        )
+
+    def test_Reservation_List_View(self):
+        response = self.client.get(reverse('reservation'))
+        self.assertEqual(response.status_code, 200)
+
