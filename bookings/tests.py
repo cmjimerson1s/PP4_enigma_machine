067b3668063065a7a1d0016e7c98e56003276827
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-16 13:44:07 +0000

    Complete test on ReservationView

--- bookings/tests.py
+++ bookings/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Reservation
+from django.utils import timezone
 from . import views
 from django.urls import reverse
 
@@ -7,20 +8,26 @@ from django.urls import reverse
 
 class BlogPostListViewTest(TestCase):
 
-    def setUp(self):
+    def setUp(self):        
         self.reservation = Reservation.objects.create(
-            customer_name = 'Test Test',
-            customer_email = 'test@gmail.com',
-            customer_phone = '+123456789123',
-            price = 500,
-            date = '2023-01-01',
-            time_slot = '14:00',
-            room_choice = 'Horror',
-            comment = 'Test',
-            user_id =  55,
+            customer_name= 'Test Test',
+            customer_email= 'test@gmail.com',
+            customer_phone= '+123456789123',
+            price= 500,
+            date= '2000-01-01',
+            time_slot= '14:00',
+            room_choice= 'Horror',
+            comment= 'Test',
+            user_id=  55,
         )
 
     def test_Reservation_List_View(self):
         response = self.client.get(reverse('reservation'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'reservations.html')
+        self.assertContains(response, self.reservation.time_slot)
+        self.assertContains(response, self.reservation.room_choice)
+        self.assertContains(response, str(self.reservation.date))
+
+
 
