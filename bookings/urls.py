6cadd015c286c43ec2478b02d868fb3c1cd2ed99
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-16 19:04:26 +0000

    Add new reservation choice template

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReservationView.as_view(), name='reservation'),
+    path('reservation_choice', views.ReservationChoice.as_view(), name='reservation_choice'),
+
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -13,7 +13,22 @@ class ReservationView(generic.ListView):
         context = super().get_context_data(**kwargs)
         date = timezone.now().date()
         context['today'] = date.strftime("%Y-%m-%d")
-        context['times'] = ['12:00', '14:00', '16:00']
+        context['times'] = ['12:00', '14:00', '16:00','18:00']
         context['rooms'] = ['Horror', 'Pirate']
 
         return context
+
+class ReservationChoice(View):
+    def post(self, request):
+        time = request.POST.get('time')
+        date = request.POST.get('date')
+        room = request.POST.get('room')
+        template = 'res_choice.html'
+
+        context = {
+            'date': date,
+            'time': time,
+            'room': room,
+        }
+
+        return render(request, template, context)
