1d6ee2a66a71d144414d8af9279277cb591c2b90
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-16 11:52:52 +0000

    Install allauth and copy templates

--- enigma_machine/settings.py
+++ enigma_machine/settings.py
@@ -41,12 +41,23 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'bootstrap5'
     'bookings',
 ]
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
