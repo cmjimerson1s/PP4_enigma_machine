0df0ee0574528cb87983f3fbcbc83cd8d6a96961
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-25 10:50:51 +0000

    Update blog app, model, templates

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,23 @@ from cloudinary.models import CloudinaryField
 
 
 class BlogPost(models.Model):
+    TAGS = [
+        ('Promotion','Promotion'),
+        ('Sale','Sale'),
+        ('News', 'News'),
+        ('Partner Advert', 'Partner Advert'),
+        ('Holiday', 'Holiday'),
+        ('B.T.S', 'B.T.S'),
+        ('Meet the Staff', 'Meet the Staff')
+    ]
+
     blog_title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    tags = models.CharField(max_length=200, choices=TAGS, null=True)
     blog_image = CloudinaryField('image', default='placeholder')
     blog_blurb = models.TextField(blank=True)
     blog_content = models.TextField()
-    posted_date = models.DateTimeField(auto_now=True)
+    posted_date = models.DateField(auto_now=True)
     meta_tags = models.CharField(max_length=255, blank=True)
 
     class Meta:
