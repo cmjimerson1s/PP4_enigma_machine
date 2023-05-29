371eb68781c4dab42d4a6c778ea6d85a2a2dab15
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-29 20:29:16 +0000

    Cleaned up rooms code

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,7 +3,8 @@ from django.views import View
 from django.urls import path
 
 urlpatterns = [
-    path('rooms', views.RoomListView.as_view(), name='rooms'),
-    path('<slug:slug>', views.RoomListViewDetail.as_view(), name='rooms_detail')
-
+    path("rooms", views.RoomListView.as_view(), name="rooms"),
+    path(
+        "<slug:slug>",
+        views.RoomListViewDetail.as_view(), name="rooms_detail"),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from bookings.models import Room
+
 
 class RoomListView(generic.ListView):
     model = Room
     queryset = Room.objects.all()
-    template_name = 'room_list.html'
+    template_name = "room_list.html"
+
 
 class RoomListViewDetail(View):
-    
     def get(self, request, slug, *args, **kwargs):
         queryset = Room.objects.all()
         detail = get_object_or_404(queryset, slug=slug)
-        template = 'room_list_detail.html'
+        template = "room_list_detail.html"
         context = {
-            'detail': detail,
+            "detail": detail,
         }
         return render(request, template, context)
 
     def post(self, request, slug, *args, **kwargs):
-        template = 'room_list_detail.html'
+        template = "room_list_detail.html"
         return render(request, template)
