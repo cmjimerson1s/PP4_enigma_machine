befc5f93443c251ac608b863e0fee6b1c9b91815
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-29 14:37:56 +0000

    Update base.html

--- enigma_machine/settings.py
+++ enigma_machine/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ["pp4-enigma-machine.herokuapp.com", "localhost", "8000-cmjimerson1-pp4enigmama-euj4cv35q5k.ws-eu97.gitpod.io"]
 
 
