526e86c220ee003ec73a35f301debdfe63a83473
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-29 14:30:10 +0000

    Update settings.py

--- enigma_machine/settings.py
+++ enigma_machine/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["pp4-enigma-machine.herokuapp.com", "localhost", "8000-cmjimerson1-pp4enigmama-euj4cv35q5k.ws-eu97.gitpod.io"]
 
@@ -159,9 +159,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
