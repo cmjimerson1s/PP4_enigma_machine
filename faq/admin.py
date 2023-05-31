2e8a4e3ec79f3925a89babd2a57935cb20845662
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-31 14:08:57 +0000

    Add faq to admin

--- faq/admin.py
+++ faq/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Faq
 
-# Register your models here.
+@admin.register(Faq)
+class FaqAdmin(admin.ModelAdmin):
+    list_display = ("question", "answer")
+    search_fields = ("question", "answer")
+    list_filter = ("question", "answer")
