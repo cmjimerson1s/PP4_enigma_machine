443a5b75a2714399706d1f1a6e34299e4a844efb
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-31 14:06:42 +0000

    Create FAQ model

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Faq(models.Model):
+    question = models.CharField(max_length=200, unique=True)
+    answer = models.CharField(max_length=500)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return {self.question}
